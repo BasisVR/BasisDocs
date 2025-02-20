c9aa5e6d29cee086846548e194924ea002fd0f15
towneh <matt.town@gmail.com>
2025-02-20 11:40:06 +0000

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,7 +24,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'BasisVR', // Usually your GitHub org/user name.
   projectName: 'BasisDocs', // Usually your repo name.
-
+  deploymentBranch: 'gh-pages',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
