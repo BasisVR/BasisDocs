5558b503edd7f8d12987d99788a4baef90b20fff
Maebbie <Maebbie@gmail.com>
2026-09-18 22:28:26 +0200

    Update PPullfile.tsx

--- content/docs/components/PPullfile.tsx
+++ content/docs/components/PPullfile.tsx
@@ -23,12 +23,12 @@ export default function PPull({ id, label, href, fetchUrl }: PList) {
       .then((text) => {
         const pre = document.createElement("pre");
         const code = document.createElement("code");
-        code.txtI = text;
+        code.textContent = text;
         pre.appendChild(code);
         status.replaceWith(pre);
       })
       .catch(() => {
-        status.querySelector("a").txtI = label;
+        status.querySelector("a").textContent = label;
       });
   }, [id, label, fetchUrl]);
 
